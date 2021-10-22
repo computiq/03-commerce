cf64ae3077aeecd43a155024a0c695057668bc37
Saba Salman <sabamsalman@gmail.com>
2021-10-22 22:05:28 +0300

    -i forgot to commit nvm

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from ninja import Router
 
-# Create your views here.
+products_controller = Router(tags = ['products'])
+adresses_controller = Router(tags = ['adresses'])
+
+@products_controller.get('/products')
+def listProducts(request):
+    return list(Product.objects.all())
+
+@adresses_controller.get('/adresses')
+def listAdresses(request):
+   return list(Address.objects.all())
+#incase someone is stalking my solution, I'm not finished c;
