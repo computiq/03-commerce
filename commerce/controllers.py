18e99ccc19bf58c59285ececdda9bd18cabf84af
yousif <yousif.b.almufti@gmail.com>
2021-10-22 19:51:32 +0300

    yousif basil
    yousif basil

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ninja import Router
+from commerce.models import Product
+from commerce.models import Address
+
+commerce_controller = Router()
+from django.core import serializers
+
+
+@commerce_controller.get('get_products')
+def get_products(request):
+    return list(Product.objects.values())
+
+
+@commerce_controller.get('get_address')
+def get_address(request):
+    return list(Address.objects.values())
+
+
