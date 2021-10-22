c29f9406476dbaa59747ae07b72a48bb7d9cf8fe
Khadija M. Al-Khashab <khadija.alkhashab@gmail.com>
2021-10-22 12:08:13 +0300

    Khadija Al-Khashab's solution

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from ninja import Router
+from commerce.models import Product, Address
 
-# Create your views here.
+product_controller = Router()
+address_controller = Router()
+
+# for the extra sub-task
+@product_controller.get('')
+def list_products(request):
+  return list(Product.objects.select_related('category').values('name', 'qty', 'discounted_price', 'category__name'))
+
+# get all addresses
+@address_controller.get('')
+def list_addresses(request):
+  return list(Address.objects.values(''))
+  '''
+  for retrieving only specific attributes just pass their names in values as arguments
+  return list(Address.objects.values('phone'))
+  '''
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from commerce.controllers import product_controller, address_controller
+
+api = NinjaAPI()
+
+api.add_router('products', product_controller)
+api.add_router('addresss', address_controller)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
