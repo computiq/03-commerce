de3b1f0ae2c24c78ffc48e0c3ed295778c781732
Karrar <karrarprogrammer01@gmail.com>
2021-10-22 18:16:46 +0300

    bonus to multi endpoint

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,5 +1,5 @@
 from ninja import Router, ModelSchema
-from commerce.models import Product, Address, User, City
+from commerce.models import Product, Address, User, City, Vendor, Merchant, Label, Category
 from ninja.orm import create_schema
 from typing import List
 
@@ -7,6 +7,7 @@ commerce_controller = Router()
 
 UserSchema = create_schema(User, exclude= ['password', 'last_login', 'user_permissions','is_superuser','groups','is_staff','is_active'])
 CitySchema = create_schema(City)
+ProductSchema = create_schema(Product, depth=1)
 
 class AddressSchema(ModelSchema):
     class Config:
@@ -15,9 +16,9 @@ class AddressSchema(ModelSchema):
     user: UserSchema
     city: CitySchema
 
-@commerce_controller.get("products")
+@commerce_controller.get("products", response=List[ProductSchema])
 def show_products(request):
-    return list(Product.objects.values())
+    return list(Product.objects.select_related("category", "vendor", "merchant", "label").all())
 
 @commerce_controller.get("addresses", response=List[AddressSchema])
 def show_addresses(request):
