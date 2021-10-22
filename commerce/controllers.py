d69576c0547dd276ed39488ed41e9a6c9bfb62e4
Rabab-Ali99 <80794290+Rabab-Ali99@users.noreply.github.com>
2021-10-22 17:59:07 +0300

    Update controllers.py

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from ninja import Router
+from commerce.models import Product, Address
+from django.core import serializers
 
-# Create your views here.
+productsController = Router()
+addressesController = Router()
+bonusTaskController = Router()
+
+
+@productsController.get('')
+def get_all_products(request):
+    queryset = Product.objects.all()
+    addresses = serializers.serialize("json", queryset)
+    return HttpResponse(addresses, content_type="application/json")
+
+
+@addressesController.get('')
+def get_all_addresses(request):
+    queryset = Address.objects.all()
+    addresses = serializers.serialize("json", queryset)
+    return HttpResponse(addresses, content_type="application/json")
+
+# still working on this
+# @bonusTaskController.get('')
+# def get_bonus(request):
+#     queryset0 = Product.objects.select_related('merchant','category','label')
+
