76204d9a889b0473a7acc4428168d5d359d51e69
Mohammed Falih <89539749+mofalih64@users.noreply.github.com>
2021-10-22 17:42:55 +0300

    finish the task

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from ninja import Router
+from commerce.models import Product,Merchant,Vendor,Category,Label,Address
+from django.core import serializers
 
-# Create your views here.
+commerce_controller=Router()
+
+@commerce_controller.get("/products")
+def get_the_Products(request):
+   data = serializers.serialize("json", Product.objects.all())
+  
+   return data
+
+@commerce_controller.get("/Address")
+def get_the_Products(request):
+   data = serializers.serialize("json", Address.objects.all())
+   return data
