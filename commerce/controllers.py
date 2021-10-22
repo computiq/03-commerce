a24af64cd6a5e3c6f21f6033d964d67cae4655e0
Sa9i9f <58704805+Sa9i9f@users.noreply.github.com>
2021-10-22 19:42:26 +0300

    done

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from ninja import Router, Schema
+from ninja.main import NinjaAPI
+from django.http import JsonResponse
+from .models import Category, Product , Address
 
-# Create your views here.
+
+
+commerce_controllers = Router()
+
+@commerce_controllers.get("products")
+def fetch_products(request):
+    result= Product.objects.values()
+    list_result = [p for p in result] 
+    return list_result
+
+
+@commerce_controllers.get("addresses")
+def fetch_address(request):
+    result= Address.objects.values()
+    list_result = [p for p in result] 
+    return list_result
+
+
+
+
+
+
+
+
+
+
+
