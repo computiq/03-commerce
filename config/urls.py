3818bdbadb2b509a441428f2bc427f2214406794
Aland <32212842+alandio@users.noreply.github.com>
2021-10-24 00:16:58 +0300

    Implement two endpoints to list products and addresses

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from commerce.controllers import products, addresses
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+api.add_router('', products)
+api.add_router('', addresses)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
