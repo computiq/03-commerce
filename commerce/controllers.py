f70de4b4115cf2107de2a0d6cfdf589650669362
hawraa majeed <hourya.majeed96@gmail.com>
2021-10-23 22:54:04 +0300

    finally

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from ninja import Router
+from typing import List
+from commerce.models import Product, Address
 
-# Create your views here.
+
+productsController = Router()
+addressesController = Router()
+
+
+
+@productsController.get('')
+def list_products(request):
+    return list(Product.objects.all().values())
+
+
+@addressesController.get('')
+def list_adderess(request):
+    return list(Address.objects.all().values())
+
+
+
+""""
+I am still thinking of a solution bonus task
+
+"""
--- commerce/models.py
+++ commerce/models.py
@@ -1,5 +1,4 @@
 import uuid
-
 from PIL.Image import Image
 from django.contrib.auth import get_user_model
 from django.db import models
