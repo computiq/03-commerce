eeca6bfca60e4751c5f54c89ebebfd8016d0b717
Mohammed Falih <89539749+mofalih64@users.noreply.github.com>
2021-10-23 16:04:18 +0300

    fix a bug in returning data

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,14 +1,9 @@
-from ninja import Router,Schema
-from commerce.models import Product,Merchant,Vendor,Category,Label,Address
-from django.core import serializers
+from ninja import Router
+from commerce.models import Product,Address
 from querybuilder.query import Query
 
 
 commerce_controller=Router()
-class the_data(Schema):
-   primary:list
-   secondary:list
-
 
 @commerce_controller.get("/products")
 def get_the_Products(request):
