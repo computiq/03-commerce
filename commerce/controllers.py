baa2620d494e988bcdd59aa011204f5333b38c90
Ameer Nashwan <ameerhadidi8765@gmail.com>
2021-10-24 00:45:26 +0300

    task done

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,13 @@
+from ninja import Router
 from django.shortcuts import render
 
-# Create your views here.
+from commerce.models import Address, Product
+
+commerce_controller = Router()
+@commerce_controller.get("products")
+def addresses(request):
+    return(list(Product.objects.values()))
+
+@commerce_controller.get("addresses")
+def addresses(request):
+    return(list(Address.objects.values()))
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
 
+from commerce.controllers import commerce_controller
+
+api = NinjaAPI()
+api.add_router("commerce", commerce_controller)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("commerce/", api.urls),
 ]
