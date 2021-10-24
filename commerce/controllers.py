0caa899c15ace7c0048dd0eb1df2dddef0f7bebc
Hiba Sattar <satar.hiba@gmail.com>
2021-10-24 18:29:52 +0300

    first trial

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ninja import Router
+from commerce.models import Address, Product
+from typing import List
+address_controller = Router()
+product_controller = Router()
+
+
+@product_controller.get('')
+def list_products(request):
+    return list(Product.objects.all().values())
+
+
+@address_controller.get('')
+def list_products(request):
+    return list(Address.objects.all().values())
