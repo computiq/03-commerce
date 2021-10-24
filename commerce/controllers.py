fe0dc29849adff1b552999bf009716266d051a49
Zahraa Basheer <68440929+zhr9797@users.noreply.github.com>
2021-10-24 22:56:50 +0300

    Update controllers.py

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from ninja import Router
+from django.http import JsonResponse
+from commerce.models import Product, Address
 
-# Create your views here.
+
+commerce_controller = Router()
+
+
+@commerce_controller.get('/api/products')
+def list_products(request):
+    result = list( Product.objects.values())
+
+    return JsonResponse(result, safe=False)
+
+
+
+@commerce_controller.get('/api/addresses')
+def list_addresses(request):
+    result = list( Address.objects.values())
+
+    return JsonResponse(result, safe=False)
+
+
+
