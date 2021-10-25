0d86d14bd65b6ddba68c6eed35ad37d3b6347a9d
ROQAYA_AMEER <roqayaameer312@gmail.com>
2021-10-25 09:29:01 +0300

    task3

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from ninja import Router, Schema
+from commerce.models import Product, Address
+from typing import List
 
-# Create your views here.
+commerce_controller = Router()
+
+
+@commerce_controller.get('')
+def get_queryset(request):
+    return list(Product.objects.values())
+
+
+@commerce_controller.get('')
+def get_address(request):
+    return list(Address.objects.values())
+
+
+class UserSchema(Schema):
+    id: int
+    first_name: str
+    description : str
+    weight : float
+    width : float
+    height : float
+    length : float
+    qty : int
+    cost : int
+    price : int
+    discounted_price : int
+class ProductSchema(Schema):
+    id: int
+    name: str
+    description : str
+    weight: float
+    width: float
+    height: float
+    length: float
+    qty: int
+    cost: int
+    price: int
+    discounted_price: int
+    merchant: UserSchema = None
+
+
+@commerce_controller.get("/Product", response=List[ProductSchema])
+def tasks(request):
+    queryset = Product.objects.all()
+    return list(queryset)
